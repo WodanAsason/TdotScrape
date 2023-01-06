26cc690468fb15e666f21fdd08bc0b652fd381cf
WodanAsason <edward@wayofthewolf.biz>
2023-01-06 01:42:28 -0500

    Added SBA, flask

--- scraping.py
+++ scraping.py
@@ -14,52 +14,51 @@ MTCC_URL = "https://www.mtccc.com/events/"
 SBA_URL = "https://www.scotiabankarena.com/calendar"
 
 
-def get_browser():
-    # Selenium Setup
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = True
-    # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox") # linux only
-    # chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(10)
-    return driver
+class Scraper:
+    def __init__(self):
+        self.browser = self.get_browser()
 
+    def get_browser(self):
+        # Selenium Setup
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.headless = True
+        # chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--no-sandbox") # linux only
+        # chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.implicitly_wait(10)
+        return driver
 
-def get_mtcc():
-    browser = get_browser()
-    browser.get(MTCC_URL)
+    def get_mtcc(self):
+        self.browser.get(MTCC_URL)
 
-    sleep(2)
-    events = browser.find_elements(By.XPATH, '//ul[@id="eventList"]/li/a')
+        sleep(2)
+        events = self.browser.find_elements(By.XPATH, '//ul[@id="eventList"]/li/a')
 
-    formatted = pd.DataFrame({
-             'title': event.find_element(By.XPATH, './/div[@class="mainEvItemTitle"]').text,
-             'link': event.get_attribute('href'),
-             'dates': event.find_element(By.XPATH, './/div[@class="alt eventDay"]').text,
-             'type': event.find_element(By.XPATH, './/div[@class="mainEvItemType"]').text
-                             } for event in events)
+        formatted = pd.DataFrame({
+                 'title': event.find_element(By.XPATH, './/div[@class="mainEvItemTitle"]').text,
+                 'link': event.get_attribute('href'),
+                 'dates': event.find_element(By.XPATH, './/div[@class="alt eventDay"]').text,
+                 'type': event.find_element(By.XPATH, './/div[@class="mainEvItemType"]').text
+                                 } for event in events)
 
-    formatted.to_csv('data/mtcc.csv')
+        formatted.to_csv('data/mtcc.csv', index=False)
 
+    def get_sba(self):
+        self.browser.get(SBA_URL)
 
-def get_sba():
-    browser = get_browser()
-    browser.get(SBA_URL)
+        events = self.browser.find_elements(By.XPATH, '//div[contains(@class, "hasEvent")]')
+        formatted = pd.DataFrame({
+            'title': event.find_element(By.XPATH, './/div[@class="event_item"]').text,
+            'link': event.find_element(By.XPATH, './/a').get_attribute('href'),
+            'dates': event.get_attribute('data-fulldate'),
+            'type': 'public'
+                                 }for event in events)
+        formatted.to_csv('data/sba.csv')
 
-    events = browser.find_elements(By.XPATH, '//div[contains(@class, "hasEvent")]')
-    formatted = pd.DataFrame({
-        'title': event.find_element(By.XPATH, './/div[@class="event_item"]').text,
-        'link': event.find_element(By.XPATH, './/a').get_attribute('href'),
-        'dates': event.get_attribute('data-fulldate'),
-        'type': 'public'
-                             }for event in events)
-    formatted.to_csv('data/sba.csv')
-
-
-def read_csv(file):
-    return pd.read_csv(f'data/{file}.csv')
+    def read_csv(self, file):
+        return pd.read_csv(f'data/{file}.csv')
 
 # # MTCC stuff
 # get_mtcc()
--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+import scraping
 import pandas as pd
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
+scraper = scraping.Scraper()
 
 
 @app.route('/')
 def home():
-    return "<h1>Temp</h1>"
+    mtcc = scraper.read_csv('mtcc').to_dict(orient='records')
+    sba = scraper.read_csv('sba').to_dict(orient='records')
+    data = [mtcc, sba]
+    return render_template('index.html', data=data)
